e601d20f4462b266e99bc63169454fcce2c24bf1
Ashwin <ashwinnagelwar@gmail.com>
2021-12-17 11:03:46 +0530

    Project created

---
+++ src/main/java/com/bikkadIt/JenkinsFirstDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bikkadIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsFirstDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsFirstDemoApplication.class, args);
+	}
+
+}
